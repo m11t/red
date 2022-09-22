1cd0731520ec1d8b041a4b43146f65f66f20b47e
Melf Lichtenberg <melfthomsen@gmail.com>
2022-09-22 15:01:33 +0200

    Fix error by providing SearchFacadeService

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,12 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
-import { SearchModule } from '@red-probeaufgabe/search';
+import { AbstractSearchFacadeService, SearchFacadeService, SearchModule } from '@red-probeaufgabe/search';
 import { UiModule } from '@red-probeaufgabe/ui';
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [CommonModule, UiModule, SearchModule, DashboardRoutingModule],
+  /**
+   * Da die DashboardComponent die abstrakte Klasse AbstractSearchFacadeService per Injection haben möchte
+   * wird ein Provider dafür benötigt.
+   * Eine abstrakte Klasse ist nicht instanziierbar, daher wird für AbstractSearchFacadeService hier der implementierte SearchFacadeService
+   * für das hier verwendete Backend verwendet.
+   */
+  providers: [
+    { provide: AbstractSearchFacadeService, useClass: SearchFacadeService }
+  ],
   exports: [DashboardComponent],
 })
 export class DashboardModule {}
